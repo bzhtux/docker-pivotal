3ad12b7ec1b98c41af8361094c183b6146ec20df
Yannick Foeillet <bzhtux@gmail.com>
2021-02-13 00:23:33 +0100

    Update goss file to match new version of om echo 7.2.0

--- test.sh
+++ test.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+echo "<--% START DOCKERFILE %-->"
 cat Dockerfile
+echo "<--% END DOCKERFILE %-->"
 
+# DGOSS tests
 dgoss run pivotal sleep 3
+
+# Trigger docker build and pubish to docker hub
+curl -X POST https://hub.docker.com/api/build/v1/source/d5cbd3b0-8269-48d1-85f2-ce14149f7e3f/trigger/c6a596e3-d753-41e2-ae1c-1d6acaa5d16b/call/
