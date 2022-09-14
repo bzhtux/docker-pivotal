429e741b150cc8224951d7fcf84a4bd3959a7cb2
Yannick Foeillet <bzhtux@gmail.com>
2022-09-14 10:57:53 +0200

    feat(docker) 🛠  Update dockeer build script

--- docker.sh
+++ docker.sh
@@ -2,18 +2,22 @@
 
 set -euo pipefail
 
+TRIGGER=${TRAVIS_TEST_RESULT:-1}
 DOCKER_TAG=${TRAVIS_TAG:-master}
 
-function login {
-    echo "*** Docker hub login"
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-}
+case $TRIGGER in
+    0) case $DOCKER_TAG in
+            master) SRC_NAME="Branch"
+                ;;
+            *) SRC_NAME="Tag"
+                ;;
+        esac
+        ;;
+    1) echo "Tests failed, no trigger set for docker hub build"
+        ;;
+esac
 
-function deploy {
-    echo "*** Deploy to Docker Hub"
-    docker build -t "bzhtux/pivotal:$DOCKER_TAG" .
-    docker push "bzhtux/pivotal:$DOCKER_TAG"
-}
-
-login
-deploy
+curl -X POST \
+        -H "Content-Type: application/json" \
+        -d "{\"source_type\": \"${SRC_NAME}\", \"source_name\": \"${DOCKER_TAG}\"}" \
+        https://hub.docker.com/api/build/v1/source/d5cbd3b0-8269-48d1-85f2-ce14149f7e3f/trigger/c6a596e3-d753-41e2-ae1c-1d6acaa5d16b/call/
